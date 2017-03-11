5fbb7c7240fa79ac514cfb347069f02241a785d3
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-03-11 13:24:13 -0600

    added comments

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,6 +19,7 @@
     	    token = result.credential.accessToken;
 
             if (token) {
+                //if we have a token from login show the input div
                 $('#add-train').show();
                 $('#help-text').hide();
             }
@@ -33,15 +34,15 @@
 		var email = error.email;
 		// The firebase.auth.AuthCredential type that was used.
 		var credential = error.credential;
-		// ...
 	});
 
 	//on click event for submit button
 	$('#submit-btn').on('click', function() {
-        
+        //check if there is a token (logged in user)
         if (token) {
     		event.preventDefault(); //prevent form from submitting and refreshing after button is clicked
 
+            //variable declaration
     		var trainName;
     		var trainDestination;
     		var firstTrainTime;
@@ -51,11 +52,13 @@
     		var timeDiff;
     		var timeRemainder;
 
+            // grabbing user input and storing in a variable
     		trainName = $('#name-input').val().trim();
     		trainDestination = $('#destination-input').val().trim();
     		firstTrainTime = $('#first-time-input').val().trim();
     		trainFrequency = $('#frequency-input').val().trim();
 
+            //clear the fields after grabbing the values
             $('#name-input').val('');
             $('#destination-input').val('');
             $('#first-time-input').val('');
@@ -68,7 +71,7 @@
     		timeRemainder = (timeDiff % trainFrequency);
     		//Next Arrival
     		minutesAway = (trainFrequency - timeRemainder);
-
+            //push values to firebase DB
     		database.ref().push({
     			trainName: trainName,
     			trainDestination: trainDestination,
@@ -77,16 +80,18 @@
     		});
 
         } else {
+            //handle situation where someone uses inspect to unhide our hidden div
             $('#add-train').hide();
             alert("You need to login before you can submit.");
         }
 	});
 
 	database.ref().on('child_added', function(childSnapShot){
+        //display values on initial load and add display new entries in the table
 		var tableRow = $('<tr>');
-
+        //used to get the current arrival on page load
 		var currentNextArrival = moment().add(childSnapShot.val().minutesAway, "minutes").format("hh:mm a");
-
+        //create table rows to display information from firebase
 		tableRow.append('<td>' + childSnapShot.val().trainName + '</td>');
 		tableRow.append('<td>' + childSnapShot.val().trainDestination + '</td>');
 		tableRow.append('<td>' + childSnapShot.val().trainFrequency + '</td>');
@@ -102,6 +107,7 @@
 }
 
 function userLogin() {
+    //called when login button is clicked to redirect to Google Signin
 	var provider = new firebase.auth.GoogleAuthProvider();
 	firebase.auth().signInWithRedirect(provider);
 }
